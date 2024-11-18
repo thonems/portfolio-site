2d6300c47646ba6c84ad31c4608598d979eda043
thonems <thonem10@gmail.com>
2024-11-18 19:09:28 +0100

    added flex and styling on header

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import {motion} from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
 
 export default function Header() {
     return (
@@ -11,10 +13,17 @@ export default function Header() {
         animate={{y:0, x:"-50%", opacity:1}}
         ></motion.div>
 
-        <nav className="fixed top-[0.2rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.5rem] sm:h-[initial] sm:py-0">
-        <ul>
-            
-        </ul>
+        <nav className="fixed flex top-[0.2rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.5rem] sm:h-[initial] sm:py-0">
+            <ul className="flex w-[20rem] flex-wrap items-center justify-center gap-y-1 text-[1rem] font-medium text-gray-600 sm:w-[initial] sm:flex-nowrap sm:gap-4">
+                {links.map((link) => (
+                        <li className="h-3/4 flex items-center justify-center" key={link.hash} >
+                            <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-900" href={link.hash}>
+                                {link.name}
+                            </Link>
+                            </li>
+                    ))
+                }
+            </ul>
         </nav>
     </header>
     );
