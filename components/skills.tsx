32a359573b6b4ebb423bddaf4536dc4a2fd1e307
Thomas Nemeth <thonem10@gmail.com>
2024-11-28 20:06:34 +0100

    skills are looking nice and done. Contact form i also looking descent, only need to send the data to email later.

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { skills } from '@/lib/data';
 
 export default function Skills(){
     return (
-        <section id='skills' className='scroll-mt-32'>
+        <section id='skills' className='scroll-mt-32 mb-24 max-w-3xl sm:mb-40 text-center'>
             <h2 className='text-3xl font-bold mb-8'>Skills</h2>
+            <ul className='flex flex-wrap justify-center gap-3 text-lg text-gray-400'>
+                {
+                    skills.map((skills, i) => (
+                        <li className="bg-white rounded-md border border-black/20 px-4 py-2" key={i}>{skills}</li>
+                    ))
+                }
+            </ul>
         </section>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  module.exports = nextConfig
