25d5b63e5ae706fe4a8ea77fa8ea326c78b66f27
Thomas Nemeth <thonem10@gmail.com>
2024-11-28 22:19:46 +0100

    added serverside  component for email

--- components/email.tsx
+++ components/email.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 export const sendEmail = async (formData: FormData) => {
+    console.log("server side: ");
     console.log(formData.get("email"));
     console.log(formData.get("message"));
 };
