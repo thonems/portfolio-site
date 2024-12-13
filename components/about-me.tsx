3aa02711e9a21608efc20d115903ca69afe37ec1
Thomas Nemeth <thonem10@gmail.com>
2024-12-13 14:48:03 +0100

    prettier

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -9,7 +9,11 @@ export default function About() {
       <h2 className="text-3xl font-bold mb-8">About me</h2>
       <p className="mb-3"></p>
       <p>
-      I graduated with a bachelor’s degree in Information Technology from Høyskolen Kristiania, where I specialized in backend development. My expertise includes working with technologies like .NET, Java, C, and SQL. I’m a lifelong learner who loves diving into new tools and frameworks, always aiming to build something exceptional.
+        I graduated with a bachelor’s degree in Information Technology from
+        Høyskolen Kristiania, where I specialized in backend development. My
+        expertise includes working with technologies like .NET, Java, C, and
+        SQL. I’m a lifelong learner who loves diving into new tools and
+        frameworks, always aiming to build something exceptional.
       </p>
     </section>
   );
--- components/projects.tsx
+++ components/projects.tsx
@@ -9,34 +9,32 @@ export default function Projects() {
       <div>
         {projects.map((project, i) => (
           <a href={project.link} key={project.title} target="_blank">
-
-          
-          <section
-            className="relative bg-gray-200 border border-black/10 sm:h-[20rem] overflow-hidden max-w-[40rem] sm:pr-8 mb-4 sm:mb-8 hover:bg-gray-400 transition flex"
-            key={project.title}
-          >
-            <div className="flex flex-col py-4 px-5 sm:pl-10 sm:pt-10 sm:pr-2 sm:max-w-[50%] h-full">
-              <h3 className="font-bold text-2xl">{project.title}</h3>
-              <p className="mt-2 leading-6 text-gray-700">
-                {project.description}
-              </p>
-              <ul className="flex flex-wrap sm:mt-auto mt-5 gap-3">
-                {project.tech.map((tech, i) => (
-                  <li
-                    key={i}
-                    className="bg-black/20 px-4 py-1 rounded-full text-sm tracking-wide text-white "
-                  >
-                    {tech}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <Image
-              src={project.image}
-              className="absolute top-8 -right-40 rounded-full w-[25rem] hidden sm:block"
-              alt={project.title}
-            />
-          </section>
+            <section
+              className="relative bg-gray-200 border border-black/10 sm:h-[20rem] overflow-hidden max-w-[40rem] sm:pr-8 mb-4 sm:mb-8 hover:bg-gray-400 transition flex"
+              key={project.title}
+            >
+              <div className="flex flex-col py-4 px-5 sm:pl-10 sm:pt-10 sm:pr-2 sm:max-w-[50%] h-full">
+                <h3 className="font-bold text-2xl">{project.title}</h3>
+                <p className="mt-2 leading-6 text-gray-700">
+                  {project.description}
+                </p>
+                <ul className="flex flex-wrap sm:mt-auto mt-5 gap-3">
+                  {project.tech.map((tech, i) => (
+                    <li
+                      key={i}
+                      className="bg-black/20 px-4 py-1 rounded-full text-sm tracking-wide text-white "
+                    >
+                      {tech}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <Image
+                src={project.image}
+                className="absolute top-8 -right-40 rounded-full w-[25rem] hidden sm:block"
+                alt={project.title}
+              />
+            </section>
           </a>
         ))}
       </div>
